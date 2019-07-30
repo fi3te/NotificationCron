9506b9976e6f10e16aecd8af55eb90f03b3bc5da
fi3te <w.fiete@web.de>
2019-07-30 21:10:30 +0200

    Apache license copy added

--- app/src/main/java/com/github/notificationcron/ui/licenses/LicensesActivity.kt
+++ app/src/main/java/com/github/notificationcron/ui/licenses/LicensesActivity.kt
@@ -2,15 +2,24 @@ package com.github.notificationcron.ui.licenses
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.notificationcron.R
+import kotlinx.coroutines.*
 
 class LicensesActivity : AppCompatActivity() {
+
+    private val job = Job()
+    private val ioScope = CoroutineScope(Dispatchers.IO + job)
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_licenses)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        ioScope.launch {
+            setLicenseText("copy_of_apache_license.txt", findViewById(R.id.apacheText))
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -20,6 +29,21 @@ class LicensesActivity : AppCompatActivity() {
                 return true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private suspend fun setLicenseText(fileName: String, textView: TextView) = withContext(Dispatchers.IO) {
+        val inputStream = assets.open(fileName)
+        val size = inputStream.available()
+
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+
+        val text = String(buffer)
+
+        uiScope.launch {
+            textView.text = text
         }
     }
 }
