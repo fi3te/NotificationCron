f4041bcf0e11439b12e758007a574f8afb449c4b
fi3te <w.fiete@web.de>
2020-01-25 13:50:48 +0100

    Notification group summary added

--- app/src/main/java/com/github/fi3te/notificationcron/ui/NotificationService.kt
+++ app/src/main/java/com/github/fi3te/notificationcron/ui/NotificationService.kt
@@ -14,6 +14,8 @@ import java.util.concurrent.atomic.AtomicInteger
 
 
 private const val NOTIFICATION_CHANNEL_ID = "NOTIFICATION_CHANNEL_ID"
+private const val NOTIFICATION_GROUP_KEY = "NOTIFICATION_GROUP_KEY"
+private const val NOTIFICATION_GROUP_SUMMARY_ID = 524195436
 private val NOTIFICATION_ID_COUNTER = AtomicInteger()
 
 fun newNotificationId(): Int {
@@ -38,12 +40,24 @@ fun createNotificationChannel(context: Context, notificationManager: Notificatio
     }
 }
 
+fun createNotificationGroupSummary(context: Context, notificationManager: NotificationManager) {
+    val notificationGroupSummary = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setGroup(NOTIFICATION_GROUP_KEY)
+        .setStyle(NotificationCompat.InboxStyle())
+        .setGroupSummary(true)
+        .build()
+    notificationManager.notify(NOTIFICATION_GROUP_SUMMARY_ID, notificationGroupSummary)
+}
+
 fun createNotification(context: Context, title: String, text: String): Notification {
     val builder = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
         .setSmallIcon(R.drawable.notification_icon)
         .setContentTitle(title)
         .setContentText(text)
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        .setGroup(NOTIFICATION_GROUP_KEY)
     return builder.build()
 }
 
@@ -52,6 +66,7 @@ fun showNotification(context: Context, title: String, text: String) {
     val notificationManager: NotificationManager =
         context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     createNotificationChannel(context, notificationManager)
+    createNotificationGroupSummary(context, notificationManager)
     notificationManager.notify(newNotificationId(), notification)
 }
 
@@ -59,5 +74,9 @@ fun showNotification(context: Context, notificationCron: NotificationCron) {
     val timePrefix: String = notificationCron.nextNotification?.let {
         "${it.format(TIME_FORMATTER)} "
     } ?: ""
-    showNotification(context, timePrefix + notificationCron.notificationTitle, notificationCron.notificationText)
+    showNotification(
+        context,
+        timePrefix + notificationCron.notificationTitle,
+        notificationCron.notificationText
+    )
 }
