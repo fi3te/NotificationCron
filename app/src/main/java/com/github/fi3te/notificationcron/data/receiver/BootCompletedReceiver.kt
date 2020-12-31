04d567e30479a5f8f1848b257c5dcab7a99b09f0
fi3te <w.fiete@web.de>
2020-12-31 20:39:05 +0100

    Refactoring

--- app/src/main/java/com/github/fi3te/notificationcron/data/receiver/BootCompletedReceiver.kt
+++ app/src/main/java/com/github/fi3te/notificationcron/data/receiver/BootCompletedReceiver.kt
@@ -1,5 +1,6 @@
 package com.github.fi3te.notificationcron.data.receiver
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -7,11 +8,12 @@ import com.github.fi3te.notificationcron.data.scheduleAlarms
 
 class BootCompletedReceiver : BroadcastReceiver() {
 
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context?, intent: Intent?) {
         if (context != null) {
-            Thread(Runnable {
+            Thread {
                 scheduleAlarms(context)
-            }).start()
+            }.start()
         }
     }
 }
