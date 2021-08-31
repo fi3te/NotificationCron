efcbeddd285953f03561261b6f814da2b90adae2
fi3te <fi3te@users.noreply.github.com>
2021-08-31 14:12:53 +0200

    Kotlin codegen for Moshi

--- app/src/main/java/com/github/fi3te/notificationcron/data/model/backup/v005/Settings.kt
+++ app/src/main/java/com/github/fi3te/notificationcron/data/model/backup/v005/Settings.kt
@@ -1,5 +1,8 @@
 package com.github.fi3te.notificationcron.data.model.backup.v005
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Settings(
     val theme: Int,
     val notificationCancellation: Boolean,
--- app/src/main/java/com/github/fi3te/notificationcron/data/remote/BackupJsonService.kt
+++ app/src/main/java/com/github/fi3te/notificationcron/data/remote/BackupJsonService.kt
@@ -5,10 +5,8 @@ import com.github.fi3te.notificationcron.data.model.backup.BackupV005
 import com.github.fi3te.notificationcron.data.model.backup.V005
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonDataException
-
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.PolymorphicJsonAdapterFactory
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 
 class BackupJsonService {
@@ -22,14 +20,13 @@ class BackupJsonService {
                 PolymorphicJsonAdapterFactory.of(Backup::class.java, "version")
                     .withSubtype(BackupV005::class.java, V005)
             )
-            .add(KotlinJsonAdapterFactory())
             .build()
 
         jsonAdapter = moshi.adapter(Backup::class.java)
     }
 
     @Throws(JsonDataException::class)
-    fun readJson(json: String) : Backup? {
+    fun readJson(json: String): Backup? {
         return jsonAdapter.fromJson(json)
     }
 
